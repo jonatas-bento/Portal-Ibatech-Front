cd49157818387cae4c245d9738f9a1e1df0b8608
Jonatas Bento <pr.jmbento@gmail.com>
2026-07-18 11:44:12 -0300

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:     'https://api.ibatech.com.br/api',
-  appName:    'IBATECH Portal',
-  version:    '1.0.0',
+  apiUrl: '/api',
+  appName: 'IBATECH Portal',
+  version: '1.0.0',
 } as const;
